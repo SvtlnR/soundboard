84fc8fa298a6f91200e2fecc8b099b64bfd1292f
SvtlnR <svet.ragulina@gmail.com>
2018-09-20 12:22:56 +0300

    Fixed errors in script.js

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
 		btnIdPlay(idbtn);
 	});
 	$("html").keydown(function(e){
-  		$('.btn').each(function(){
-  			if($(this).attr("data-key")==e.keyCode){
-  				var idbtn="#"+$(this).attr("id");
-  				btnIdPlay(idbtn)
-  			}
-  		})
-  	});
+		$(".btn").each(function(){
+			if($(this).attr("data-key")==e.keyCode){
+				var idbtn="#"+$(this).attr("id");
+				btnIdPlay(idbtn);
+			}
+		});
+	});
 	function btnIdPlay(idbtn){
 		var audio=$(idbtn).find("audio");
-		audio.trigger('play');	
+		audio.trigger("play");	
 	}
 });
